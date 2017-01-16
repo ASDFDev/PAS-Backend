aa8abfd97421ef79470eba942f249a73fda4f89f
Daniel Quah <danielquah148@yahoo.com>
2017-01-16 12:43:29 +0800

    Fix semicolon derp

--- AttendanceCodeAuthentication.php
+++ AttendanceCodeAuthentication.php
@@ -1,8 +1,9 @@
 <?php
 
-	define('HOST', 'localhost');
-	define('USER, 'root')
+	define('HOST','localhost');
+	define('USER', 'root');
 	define('PASS', 'hunter2');
 	define('DB', 'ats_attendance');
 	
 	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+    
