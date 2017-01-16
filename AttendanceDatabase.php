19f6bc3203d8bac0db0916c81f2224015dafb934
Daniel Quah <danielquah148@yahoo.com>
2017-01-16 14:40:53 +0800

    Add http response code for client side handling

--- AttendanceDatabase.php
+++ AttendanceDatabase.php
@@ -6,7 +6,9 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
  $attendance_code = $_POST['attendance_code'];
  
  if($device_id == '' || $username == '' || $attendance_code == ''){
-
+     echo 'Fatal error! Empty input!';
+     // Bad Request
+     http_response_code(400);
  }else{
  
  require_once('AttendanceCodeAuthentication.php');
@@ -17,8 +19,9 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
  
  if(isset($check)){
 
- echo 'You have already submitted attendance code for this lesson!';
-
+     echo 'You have already submitted attendance code for this lesson!';
+     // Too Many Requests
+     http_response_code(429);
  }else{
 
  $sql = "INSERT INTO ats (device_id,username,attendance_code) VALUES('$device_id','$username','$attendance_code')";
@@ -26,15 +29,21 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
  if(mysqli_query($con,$sql)){
 
 	echo 'Attendance submitted!';
-
+	// OK
+    http_response_code(200);
  }else{
 	echo 'An error has occured, please try again!';
+    // Internal Server Error
+	http_response_code(500);
  }
  
 	}
 	mysqli_close($con);
 	}
   }
-	else{
+else{
 	echo 'Error, unable to connect to database.';
+    // User is not using POST
+    // Method not allowed
+	http_response_code(405);
 }
