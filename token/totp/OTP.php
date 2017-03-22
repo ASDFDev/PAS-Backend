de60f7c82fcf5a6980cc64fc788b1e32103e11b3
Daniel Quah <danielquah148@yahoo.com>
2017-03-22 23:10:09 +0800

    Add parameters to OTP generator

--- token/totp/OTP.php
+++ token/totp/OTP.php
@@ -5,11 +5,12 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use OTPHP\TOTP;
 
 $totp = new TOTP(
-    "Proxmity Attendance System", 
-    "xxxx" // Input your secret key here!
+    "PAS",
+    "xxx", // Input your secret key here!
+    20,
+    'sha512',
+    8
 );
 
-$totp ->setIssuer('PAS');
+$totp ->setIssuer('Proximity Attendance System');
 echo "Current OTP: " , $totp->now() , PHP_EOL;
-
-
